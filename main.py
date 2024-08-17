d3a92ded6d3680d566438956ed899d3c28f4c9c9
SmiJa <SmiJa13@gmail.com>
2024-08-16 18:24:01 -0700

    create func to return word count

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+def word_count(text):
+  words = len(text.split())
+  return words
+
 def main():
   with open("books/frankenstein.txt") as f:
     file_contents = f.read()
-    print(file_contents)
+    # print(file_contents)
+    print(word_count(file_contents))
 
 if __name__ == '__main__':
   main()
