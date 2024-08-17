82e5fa29361519204e279c7c266ebf432f778b94
SmiJa <SmiJa13@gmail.com>
2024-08-16 21:34:53 -0700

    build report of char count

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+def main():
+  with open("books/frankenstein.txt") as f:
+    file_contents = f.read()
+    total_words = word_count(file_contents)
+    char_count = char_counts(file_contents)
+    char_list = []
+    dict_to_list(char_count, char_list)
+    char_list.sort(reverse=True, key=sort_char_count_on)
+    build_report(char_list, total_words)
+
 def word_count(text):
   words = len(text.split())
   return words
@@ -12,12 +22,23 @@ def char_counts(string):
       chars[char] = 1
   return chars
 
-def main():
-  with open("books/frankenstein.txt") as f:
-    file_contents = f.read()
-    # print(file_contents)
-    # print(word_count(file_contents))
-    print(char_counts(file_contents))
+def dict_to_list(dict, list):
+  for char in dict:
+    my_dict = {"char": char, "num": dict[char]}
+    if char.isalpha():
+      list.append(my_dict)
+
+def sort_char_count_on(dict):
+  return dict["num"]
+
+def build_report(list, words):
+  print("--- Begin report of books/frankenstein.txt ---")
+  print(f"{words} words found in the document\n")
+  for char in list:
+    letter = char["char"]
+    number = char["num"]
+    print(f"The '{letter}' character was found {number} times")
+  print("--- End report ---")
 
 if __name__ == '__main__':
   main()
